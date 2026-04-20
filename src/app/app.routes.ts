f7913f0b5160d8b698be1a3ba3bd1522c2f0b4fd
Adrian140604 <adriansivianes88@gmail.com>
2026-04-20 19:30:57 +0200

    correccion de enlace

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
   { path: 'register', component: Register },
   { path: 'login', component: Login },
   {path:"libros",component:Books,canActivate: [authGuardGuard]},
-    { path: 'forgot-password', component: ForgotPassowrd }
+  { path: 'forgot-password', component: ForgotPassowrd }
 
 
 ];
--- src/app/shared/login/login.ts
+++ src/app/shared/login/login.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef, Component, inject, NgZone } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth-service';
 import { ApiError, LoginRequest } from '../../../interfaces/types';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule,RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
